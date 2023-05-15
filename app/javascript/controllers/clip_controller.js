72973bd4b3e8d5693154a10e5eb73a775278eb06
Lucas Gruner <lucas.gruner@gmail.com>
2023-05-15 21:14:32 +0200

    favorite button is AJAX

--- app/javascript/controllers/clip_controller.js
+++ app/javascript/controllers/clip_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["item"]
   connect() {
-    console.log("hello")
+    console.log("Clip controller connected")
   }
 
   copy() {
